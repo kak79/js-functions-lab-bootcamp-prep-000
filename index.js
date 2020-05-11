7a003f573c27f0066c832352a5cf6d3f03ac50b4
Kimberly A Kohel <kohelka@gmail.com>
2020-05-11 17:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function happyHolidays(string){
 function happyHolidaysTo(string){
   var name = "you";
   console.log(`Happy holidays, ${name}!`)
-  }
   
+}
+
+function happyHolidaysTo(string){  
   var name = "everyone";
   console.log(`Happy holidays, ${name}!`)
-  
-    
   
 }
